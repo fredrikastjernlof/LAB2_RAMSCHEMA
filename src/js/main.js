f93ea4ea8cd54d9ea3c16b24cca89a9baa7bcca0
fredrikastjernlof <fredrika.stjernlof@live.se>
2026-02-03 15:00:49 +0100

    Loopar genom hämtade kurser och loggar dem i konsolen

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,34 @@
 "use strict"
 
+/* url till JSON-filen som innehåller ramschemat  */
 const url = "https://webbutveckling.miun.se/files/ramschema.json";
 
+/*Array som ska hålla alla kurser med tillhörande data från JSON-filen */
 let allCourses = [];
 
-async function fetchCourses(){
-    console.log("fetchCourses startar");
+/* Asynkron funktion som hämtar kursdata från servern */
+async function fetchCourses() {
 
-    try{
+    try {
+        /* Försöker hämta data från url:en */
+        const response = await fetch(url);
 
-    const response = await fetch(url);
-    console.log("Respons:", response);
+        /* Gör om svaret från JSON till JavaScript-objekt */
+        const data = await response.json();
 
-    const data = await response.json();
-    console.log("JSON-data:", data);
+        /* Hämtad data sparas i variabeln allCourses */
+        allCourses = data;
 
-    allCourses = data;
-    console.log("Sparar data i allCourses:", allCourses);
+        /* Loopar igenom alla kurser */
+        allCourses.forEach((course, index) => {
+            console.log(`${index + 1}. ${course.code}| ${course.coursename} | ${course.progression}`);
+        });
 
-    console.log("Kurskod:", allCourses[0].code);
-    console.log("Kursnamn:", allCourses[0].coursename);
-    console.log("Progression:", allCourses[0].progression);
-
+        /* Fångar eventuella fel */
     } catch (error) {
-        console.log ("Något gick fel vid fetch");
-        console.error(error);
+        console.error("Något gick fel vid fetch", error);
     }
-
 }
 
+/* Anropar funktionen när sidan laddas */
 fetchCourses()
