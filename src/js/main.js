9845d5fa80a73cfe17b3db27f64dc69d5c38a572
fredrikastjernlof <fredrika.stjernlof@live.se>
2026-02-03 15:41:28 +0100

    Lägger till alla kurser i tabellen efter hämtning av data

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,9 @@ const url = "https://webbutveckling.miun.se/files/ramschema.json";
 /*Array som ska hålla alla kurser med tillhörande data från JSON-filen */
 let allCourses = [];
 
+/* Hämtar <tbody> från DOM */
+const tbody = document.querySelector("#tbody");
+
 /* Asynkron funktion som hämtar kursdata från servern */
 async function fetchCourses() {
 
@@ -19,10 +22,36 @@ async function fetchCourses() {
         /* Hämtad data sparas i variabeln allCourses */
         allCourses = data;
 
-        /* Loopar igenom alla kurser */
-        allCourses.forEach((course, index) => {
-            console.log(`${index + 1}. ${course.code}| ${course.coursename} | ${course.progression}`);
+        /* Rensar tabellen */
+        tbody.innerHTML = "";
+
+        /* Loopar genom alla kursen och lägger till dom i tabellen */
+        allCourses.forEach((course) => {
+
+            /* Skapar en tabellrad */
+            const tr = document.createElement("tr");
+
+            /* Skapar tre celler till min tabell */
+            const tdCode = document.createElement("td");
+            tdCode.textContent = course.code;
+
+            const tdName = document.createElement("td");
+            tdName.textContent = course.coursename;
+
+            const tdProgression = document.createElement("td");
+            tdProgression.textContent = course.progression;
+
+            /* Lägger cellerna i en rad */
+            tr.appendChild(tdCode);
+            tr.appendChild(tdName);
+            tr.appendChild(tdProgression);
+
+            /* Lägger raden i tabellens body */
+            tbody.appendChild(tr);
+
         });
+
+        console.log("Alla kurser visas i tabellen");
 
         /* Fångar eventuella fel */
     } catch (error) {
